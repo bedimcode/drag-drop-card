8272e0c16db206a04296be21cf163e40ee3f3a60
bedimcode <61248995+bedimcode@users.noreply.github.com>
2020-11-07 00:01:54 -0500

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+/*===== DRAG and DROP =====*/
+const dropItems = document.getElementById('drop-items')
+
+new Sortable(dropItems, {
+    animation: 350,
+    chosenClass: "sortable-chosen",
+    dragClass: "sortable-drag",
+});
