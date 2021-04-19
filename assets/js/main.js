fff979e30c3da7fda84555f28a0a88681cd55d12
bedimcode <61248995+bedimcode@users.noreply.github.com>
2021-04-18 19:37:20 -0500

    Update main.js
    Save card positions in localstorage

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,4 +5,17 @@ new Sortable(dropItems, {
     animation: 350,
     chosenClass: "sortable-chosen",
     dragClass: "sortable-drag",
+    store: {
+    	// We keep the order of the list
+    	set: (sortable) =>{
+    		const order = sortable.toArray()
+    		localStorage.setItem(sortable.options.group.name, order.join('|'))
+    	},
+
+    	// We get the order of the list
+    	get: (sortable) =>{
+    		const order = localStorage.getItem(sortable.options.group.name)
+    		return order ? order.split('|') : []
+    	}
+    }
 });
